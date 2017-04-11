1056659076a17651d7f79166f90e89b485f4cc8c
ILikeBananaPie <fishyniden@gmail.com>
2017-04-11 15:24:09 +0200

    Babs

---
+++ TournementAndAnalysis/Mock-up/Form4GManipulator.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Mock_up
+{
+    partial class Form4GManipulator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.Update = new System.Windows.Forms.Button();
+            this.PlayRound = new System.Windows.Forms.Button();
+            this.Checker = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Example1 New Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(127, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Example2 New Name";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 66);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(127, 20);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "Example3 New Name";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 93);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(127, 20);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = "Example4 New Name";
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(174, 35);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(75, 23);
+            this.Update.TabIndex = 4;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // PlayRound
+            // 
+            this.PlayRound.Location = new System.Drawing.Point(13, 168);
+            this.PlayRound.Name = "PlayRound";
+            this.PlayRound.Size = new System.Drawing.Size(75, 23);
+            this.PlayRound.TabIndex = 5;
+            this.PlayRound.Text = "Play Round";
+            this.PlayRound.UseVisualStyleBackColor = true;
+            this.PlayRound.Click += new System.EventHandler(this.PlayRound_Click);
+            // 
+            // Checker
+            // 
+            this.Checker.Enabled = true;
+            this.Checker.Tick += new System.EventHandler(this.Checker_Tick);
+            // 
+            // Form4GManipulator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.PlayRound);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form4GManipulator";
+            this.Text = "Form4GManipulator";
+            this.Load += new System.EventHandler(this.Form4GManipulator_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button PlayRound;
+        private System.Windows.Forms.Timer Checker;
+    }
+}
