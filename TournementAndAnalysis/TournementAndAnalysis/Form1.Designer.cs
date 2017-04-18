5265d7e6f57ec74a4ef563365999fc33192e3903
Nitro <archi1990@hotmail.com>
2017-04-18 17:06:36 +0200

    Updated league tournament

--- TournementAndAnalysis/TournementAndAnalysis/Form1.Designer.cs
+++ TournementAndAnalysis/TournementAndAnalysis/Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.CupButton = new System.Windows.Forms.Button();
             this.LeagueButton = new System.Windows.Forms.Button();
+            this.LeagueTeams = new System.Windows.Forms.ListBox();
+            this.AddTeamTextbox = new System.Windows.Forms.TextBox();
+            this.AddTeam = new System.Windows.Forms.Button();
+            this.StartTournament = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.LeagueNextRound = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // timer1
@@ -58,17 +64,86 @@
             this.LeagueButton.UseVisualStyleBackColor = true;
             this.LeagueButton.Click += new System.EventHandler(this.LeagueButton_Click);
             // 
+            // LeagueTeams
+            // 
+            this.LeagueTeams.FormattingEnabled = true;
+            this.LeagueTeams.Location = new System.Drawing.Point(231, 230);
+            this.LeagueTeams.Name = "LeagueTeams";
+            this.LeagueTeams.Size = new System.Drawing.Size(120, 95);
+            this.LeagueTeams.TabIndex = 2;
+            this.LeagueTeams.Visible = false;
+            // 
+            // AddTeamTextbox
+            // 
+            this.AddTeamTextbox.Location = new System.Drawing.Point(231, 158);
+            this.AddTeamTextbox.Name = "AddTeamTextbox";
+            this.AddTeamTextbox.Size = new System.Drawing.Size(100, 20);
+            this.AddTeamTextbox.TabIndex = 3;
+            this.AddTeamTextbox.Visible = false;
+            // 
+            // AddTeam
+            // 
+            this.AddTeam.Location = new System.Drawing.Point(199, 370);
+            this.AddTeam.Name = "AddTeam";
+            this.AddTeam.Size = new System.Drawing.Size(98, 23);
+            this.AddTeam.TabIndex = 4;
+            this.AddTeam.Text = "Add Contestant";
+            this.AddTeam.UseVisualStyleBackColor = true;
+            this.AddTeam.Visible = false;
+            this.AddTeam.Click += new System.EventHandler(this.AddTeam_Click);
+            // 
+            // StartTournament
+            // 
+            this.StartTournament.Location = new System.Drawing.Point(199, 399);
+            this.StartTournament.Name = "StartTournament";
+            this.StartTournament.Size = new System.Drawing.Size(98, 23);
+            this.StartTournament.TabIndex = 5;
+            this.StartTournament.Text = "Start Tournament";
+            this.StartTournament.UseVisualStyleBackColor = true;
+            this.StartTournament.Visible = false;
+            this.StartTournament.Click += new System.EventHandler(this.StartTournament_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(517, 170);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 20);
+            this.tableLayoutPanel1.TabIndex = 6;
+            this.tableLayoutPanel1.Visible = false;
+            // 
+            // LeagueNextRound
+            // 
+            this.LeagueNextRound.Location = new System.Drawing.Point(477, 286);
+            this.LeagueNextRound.Name = "LeagueNextRound";
+            this.LeagueNextRound.Size = new System.Drawing.Size(75, 23);
+            this.LeagueNextRound.TabIndex = 7;
+            this.LeagueNextRound.Text = "Play Next Round";
+            this.LeagueNextRound.UseVisualStyleBackColor = true;
+            this.LeagueNextRound.Visible = false;
+            this.LeagueNextRound.Click += new System.EventHandler(this.LeagueNextRound_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(947, 455);
+            this.Controls.Add(this.LeagueNextRound);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.StartTournament);
+            this.Controls.Add(this.AddTeam);
+            this.Controls.Add(this.AddTeamTextbox);
+            this.Controls.Add(this.LeagueTeams);
             this.Controls.Add(this.LeagueButton);
             this.Controls.Add(this.CupButton);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -77,6 +152,12 @@
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Button CupButton;
         private System.Windows.Forms.Button LeagueButton;
+        private System.Windows.Forms.ListBox LeagueTeams;
+        private System.Windows.Forms.TextBox AddTeamTextbox;
+        private System.Windows.Forms.Button AddTeam;
+        private System.Windows.Forms.Button StartTournament;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button LeagueNextRound;
     }
 }
 
--- TournementAndAnalysis/TournementAndAnalysis/Form1.cs
+++ TournementAndAnalysis/TournementAndAnalysis/Form1.cs
@@ -15,6 +15,11 @@ namespace TournementAndAnalysis
         private Graphics dc;
         private Font f;
         private Random rnd = new Random();
+        private List<Button> MainScreenButtons;
+        private List<Button> LeagueAddTeamButtons;
+        private List<string> LeagueTeamList = new List<string>();
+        private List<string> ColumnHeaders;
+        private List<Button> LeagueTeamButtons = new List<Button>();
 
         public Form1()
         {
@@ -23,8 +28,18 @@ namespace TournementAndAnalysis
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            MainScreenButtons = new List<Button> { CupButton, LeagueButton };
+            LeagueAddTeamButtons = new List<Button> { AddTeam, StartTournament };
+            ColumnHeaders = new List<string> { "Contestant:", "Played:", "Won:", "Loss:", "Draw:", "Position:" };
+
             LeagueButton.Location = new Point(Width - Width / 3 - LeagueButton.Width / 2, Height / 2 - LeagueButton.Height / 2);
             CupButton.Location = new Point(Width / 3 - CupButton.Width / 2, Height / 2 - CupButton.Height / 2);
+
+            AddTeam.Location = new Point(Width / 3 - AddTeam.Width / 2, Height / 2 - AddTeam.Height / 2);
+            StartTournament.Location = new Point(Width - Width / 3 - StartTournament.Width / 2, Height / 2 - StartTournament.Height / 2);
+
+            LeagueTeams.Location = new Point(Width / 2 - LeagueTeams.Width / 2, Height / 2 + (LeagueTeams.Height / 2));
+            AddTeamTextbox.Location = new Point(Width - Width / 2 - AddTeamTextbox.Width / 2, Height / 2 - AddTeamTextbox.Height / 2);
 
             timer1.Enabled = true;
             TestMethod();
@@ -56,6 +71,81 @@ namespace TournementAndAnalysis
         }
 
         private void LeagueButton_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in MainScreenButtons)
+            {
+                button.Visible = false;
+            }
+
+            foreach (Button button in LeagueAddTeamButtons)
+            {
+                button.Visible = true;
+            }
+            AddTeamTextbox.Visible = true;
+            LeagueTeams.Visible = true;
+        }
+
+        private void AddTeam_Click(object sender, EventArgs e)
+        {
+            LeagueTeamList.Add(AddTeamTextbox.Text);
+            LeagueTeams.Items.Add(AddTeamTextbox.Text);
+            AddTeamTextbox.Text = "";
+        }
+
+        private void StartTournament_Click(object sender, EventArgs e)
+        {
+            foreach (Button button in LeagueAddTeamButtons)
+            {
+                button.Visible = false;
+            }
+            AddTeamTextbox.Visible = false;
+            LeagueTeams.Visible = false;
+            SetupTable();
+
+        }
+
+        private void SetupTable()
+        {
+            tableLayoutPanel1.Size = new Size(20, 100);
+            tableLayoutPanel1.BackColor = Color.Transparent;
+            tableLayoutPanel1.Visible = true;
+
+            tableLayoutPanel1.AutoSize = true;
+            tableLayoutPanel1.ColumnStyles.RemoveAt(0);
+            //tableLayoutPanel1.RowStyles.RemoveAt(0);
+            tableLayoutPanel1.Padding = new Padding(3);
+            tableLayoutPanel1.ColumnCount = 6;
+            for (int i = 0; i < ColumnHeaders.Count; i++)
+            {
+                Label myLabel = new Label();
+                myLabel.Text = ColumnHeaders[i];
+                tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 100));
+                tableLayoutPanel1.Controls.Add(myLabel, i, 0);
+            }
+
+
+            for (int i = 0; i < LeagueTeamList.Count; i++)
+            {
+                Button myButton = new Button();
+                myButton.Text = LeagueTeamList[i];
+                LeagueTeamButtons.Add(myButton);
+                tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 30));
+                tableLayoutPanel1.Controls.Add(myButton, 0, i + 1);
+            }
+            tableLayoutPanel1.Location = new Point(20, 20);
+
+            for (int x = 1; x < LeagueTeamList.Count + 1; x++)
+            {
+                for (int y = 1; y < 6; y++)
+                {
+                    Label myLabel = new Label();
+                    myLabel.Text = "0";
+                    tableLayoutPanel1.Controls.Add(myLabel, y, x);
+                }
+            }
+        }
+
+        private void LeagueNextRound_Click(object sender, EventArgs e)
         {
 
         }
