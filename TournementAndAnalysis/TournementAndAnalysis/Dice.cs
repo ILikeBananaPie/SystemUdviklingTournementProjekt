f050991cb643e50c00c64a8141c343ed76f57123
Steffen Schjønning <steffentidus@hotmail.com>
2017-04-21 14:25:47 +0200

    nothing

--- TournementAndAnalysis/TournementAndAnalysis/Dice.cs
+++ TournementAndAnalysis/TournementAndAnalysis/Dice.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace TournementAndAnalysis
 {
@@ -17,7 +18,19 @@ namespace TournementAndAnalysis
 
         public int Roll()
         {
+            foreach (Button btn in TournementHolder.Instance.buttons)
+            {
+                if (btn.Name.Contains("Hold") && btn.Name.Contains("Ny"))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return rnd.Next(1, 6);
+                }
+            }
             return rnd.Next(1, 6);
+            
         }
 
         public int StaticRoll(int a)
